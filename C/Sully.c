632490ec0f2b7315be5c35399eb9ed0d28f3992a
lletourn <lletourn@f6r7s6.paris.42.school>
2026-05-18 13:31:20 +0200

    Fixed sully.c extra indentation

--- C/Sully.c
+++ C/Sully.c
@@ -11,7 +11,7 @@ char cmd[128];
 void *file;
 snprintf(filename, sizeof(filename), "Sully_%d.c", i);
 file=fopen(filename, "w");
-char *s="#include <stdio.h>%2$c#include <stdlib.h>%2$cint main(){%2$cint i=%4$d;%2$cif (i<=0){return 0;}%2$c#ifdef DECREASE%2$ci--;%2$c#endif%2$cchar filename[64];%2$cchar cmd[128];%2$cvoid *file;%2$csnprintf(filename, sizeof(filename), %3$cSully_%%d.c%3$c, i);%2$cfile=fopen(filename, %3$cw%3$c);%2$c	char *s=%3$c%1$s%3$c;%2$cfprintf(file,s,s,10,34,i);%2$c	fclose(file);%2$csnprintf(cmd, sizeof(cmd), %3$cgcc -D DECREASE %%s -o Sully_%%d%3$c, filename, i);%2$csystem(cmd);%2$csnprintf(cmd, sizeof(cmd), %3$c./Sully_%%d%3$c, i);%2$csystem(cmd);}%2$c";
+char *s="#include <stdio.h>%2$c#include <stdlib.h>%2$cint main(){%2$cint i=%4$d;%2$cif (i<=0){return 0;}%2$c#ifdef DECREASE%2$ci--;%2$c#endif%2$cchar filename[64];%2$cchar cmd[128];%2$cvoid *file;%2$csnprintf(filename, sizeof(filename), %3$cSully_%%d.c%3$c, i);%2$cfile=fopen(filename, %3$cw%3$c);%2$cchar *s=%3$c%1$s%3$c;%2$cfprintf(file,s,s,10,34,i);%2$cfclose(file);%2$csnprintf(cmd, sizeof(cmd), %3$cgcc -D DECREASE %%s -o Sully_%%d%3$c, filename, i);%2$csystem(cmd);%2$csnprintf(cmd, sizeof(cmd), %3$c./Sully_%%d%3$c, i);%2$csystem(cmd);}%2$c";
 fprintf(file,s,s,10,34,i);
 fclose(file);
 snprintf(cmd, sizeof(cmd), "gcc -D DECREASE %s -o Sully_%d", filename, i);
